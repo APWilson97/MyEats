b61a6aa9d7fd41c69c06fd79ea17c392ab41dedf
Alexander Wilson <apwilson97@gmail.com>
2024-06-09 18:20:27 +0100

    added foodPage component to display information for specific food items on a separate page with add to car button, styled and placeholder function in foodService to retrieve specific food by foodId

--- frontend/src/AppRoutes.js
+++ frontend/src/AppRoutes.js
@@ -1,13 +1,15 @@
 import React from 'react'
 import { Routes, Route } from 'react-router-dom'
 import HomePage from './Pages/Home/HomePage'
+import FoodPage from './Pages/Home/Food/FoodPage'
 
 export default function AppRoutes() {
   return (
     <Routes>
-        <Route path='/' element={<HomePage/>} />
-        <Route path='/search/:searchTerm' element={<HomePage/>} />
-        <Route path='/tag/:tag' element={<HomePage/>} />
+        <Route path='/' element={<HomePage />} />
+        <Route path='/search/:searchTerm' element={<HomePage />} />
+        <Route path='/tag/:tag' element={<HomePage />} />
+        <Route path='/food/:id' element={<FoodPage />} />
     </Routes>
   )
 }
